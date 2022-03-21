946ebe75c027091368155ed00cd5de2d7400f739
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-03-21 14:30:25 +0300

    models registration

--- neighborhood/admin.py
+++ neighborhood/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Post, Profile, Alerts, Shop, Administration, Comment,Neighborhood,Health,Healthservice
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Profile)
+admin.site.register(Alerts)
+admin.site.register(Shop)
+admin.site.register(Administration)
+admin.site.register(Comment)
+admin.site.register(Neighborhood)
+admin.site.register(Health)
+admin.site.register(Healthservice)

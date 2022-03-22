23394070e2e4a01f3e4e902caa4d84b868b51ad0
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-03-23 01:48:37 +0300

    added updates to views and urls

--- neighborhood/urls.py
+++ neighborhood/urls.py
@@ -3,16 +3,16 @@ from . import views
 
 urlpatterns = [
     
-    path('', views.home, name='index'),
-    path('post/', views.new_post, name='post'),
+    path('', views.index, name='index'),
+    path('home/', views.home, name='home'),    
+    path('post/new', views.new_post, name='new_post'),
     path('view/post/<int:id>', views.view_post, name='view_post'),
     path('profile/', views.profile, name='profile'),
-    path('profile/edit', views.edit_profile, name='edit_profile'),    
+    path('profile/<username>/edit', views.edit_profile, name='edit_profile'),    
     path('search/', views.search_business, name='search'),
     path('alerts/', views.alerts, name='alerts'),
     path('new/alert/', views.new_alert, name='new_alert'),    
     path('business/', views.shops, name='business'),
     path('new/business/', views.new_shop, name='new_business'), 
-       
     path('logout', views.logout, name='logout'),
 ]
--- neighborhood/views.py
+++ neighborhood/views.py
@@ -1,4 +1,6 @@
 from multiprocessing import context
+
+import neighborhood
 from .forms import Neighbohoodform,ProfileForm, PostForm,CommentForm,AlertsForm,ShopForm
 from .models import *
 from django.shortcuts import render, redirect, HttpResponseRedirect
@@ -9,13 +11,23 @@ from django.contrib import messages
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
-def home( request):
+def index( request):
     
     title = 'Neighborhood'
     context ={
         'title': title,        
     }
     return render(request, 'index.html', context)
+
+@login_required(login_url='/accounts/login/')
+def home(request):
+    current_user =request.user
+    neighborhood_id = Profile.objects.get(user=current_user.id)
+    N_hoods=Neighborhood.get_neighborhoods
+    business=Shop.get_business_by_neighborhood(neighborhood_id.neighborhood)
+    post=Post.objects.all()
+    context={"posts":post,"user":current_user,"hoods":N_hoods,"business":business}
+    return render(request, 'home.html',context)
 
 
 @login_required(login_url='/accounts/login/')
@@ -42,53 +54,68 @@ def view_post(request,post_id):
 
 def new_post(request):
     current_user = request.user
-    profile =Profile.objects.get(user=current_user)
-
+    profile =Profile.objects.filter(user=current_user.id) 
+    
     if request.method=="POST":
         post_form =PostForm(request.POST,request.FILES)
-        if form.is_valid():
-            post = form.save(commit = False)
-            post.user = current_user
-            post.neighborhood = profile.neighborhood
-            post.profile_image = profile.profile_image
+        print()
+        if post_form.is_valid():
+            post = post_form.save(commit = False)
+            post.username = current_user
+            # post.neighborhood = profile.neighborhood
+            # post.profile_image = profile.profile_image
             post.save()
 
-        return HttpResponseRedirect('/')
+        return redirect('home')
 
     else:
-        form = PostForm()
-
-    return render(request,'post_form.html',{"form":form})
+        post_form = PostForm()
+    title = " New Post"
+    return render(request,'post_item.html',{"form":post_form, 'title': title})
 
 
 @login_required(login_url='login')
 def profile(request,userID=None):
     if userID== None:
         userID=request.user.id
-    current_user= User.objects.get(id=userID)
+    # current_user= User.objects.get(id=userID)
+    current_user= request.user
     user = current_user
     own_posts= Post.objects.filter(username=current_user)
     profile = Profile.objects.all()
-    own_alerts = Alerts.objects.filter(username=current_user)
-    context ={'posts':own_posts, 'alerts':own_alerts, 'profile':profile}
+    own_alerts = Alerts.objects.filter(author=current_user)
+    context ={'posts':own_posts, 'alerts':own_alerts, 'profile':profile,'current_user': current_user}
     return render(request, 'profile.html', context)
 
 
 
 @login_required(login_url='login')
 def edit_profile(request, username):
-    user = User.objects.get(username=username)
-    if request.method == 'POST':
-        profile_form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
-        if  profile_form.is_valid():
-            profile_form.save()
-            return redirect('/profile/', user.username)
+    # user = User.objects.get(username=username)
+    current_user = request.user  
+    get_profile= Profile.objects.filter(user_id=request.user.id)
+    if get_profile.exists():        
+        if request.method == 'POST':
+            profile_form = ProfileForm(request.POST, request.FILES)
+            if  profile_form.is_valid():
+                profile_form.save()
+                return redirect('/profile/', current_user)
+        else:        
+            profile_form = ProfileForm(instance=request.user.profile)       
     else:
-        profile_form = ProfileForm(instance=request.user.profile)
+        if request.method == 'POST':
+            profile_form = ProfileForm(request.POST, request.FILES)
+            if  profile_form.is_valid():
+                profile_form.save()
+                return redirect('/profile/', current_user)
+        else:        
+            profile_form = ProfileForm() 
+           
     context = {
-        'prof_form': profile_form
+        'prof_form': profile_form, 
+        'current_user': current_user
     }
-    return render(request, 'edit_profile.html', context)
+    return render(request, 'profile_edit.html', context)
 
 @login_required(login_url='/accounts/login/')
 def shops(request):

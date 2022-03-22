38e4ca11a8aeacbd1436e31aec7711647124ec6d
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-03-23 01:46:53 +0300

    Added necessary template

--- village/settings.py
+++ village/settings.py
@@ -142,19 +142,35 @@ MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-
+LOGIN_REDIRECT_URL= 'profile'
 
 # Get .env variables
-SECRET_KEY =os.environ.get('SECRET_KEY')
-ACCOUNT_ACTIVATION_DAYS= os.environ.get('ACCOUNT_ACTIVATION_DAYS')
-DEFAULT_FROM_EMAIL=os.environ.get('DEFAULT_FROM_EMAIL')
-EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_HOST_USER=os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST=os.environ.get('EMAIL_HOST')
-EMAIL_PORT= os.environ.get('EMAIL_PORT')
+# SECRET_KEY =os.environ.get('SECRET_KEY')
+# ACCOUNT_ACTIVATION_DAYS= os.environ.get('ACCOUNT_ACTIVATION_DAYS')
+# DEFAULT_FROM_EMAIL=os.environ.get('DEFAULT_FROM_EMAIL')
+# EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_HOST_PASSWORD')
+# EMAIL_HOST_USER=os.environ.get('EMAIL_HOST_USER')
+# EMAIL_HOST=os.environ.get('EMAIL_HOST')
+# EMAIL_PORT= os.environ.get('EMAIL_PORT')
+# EMAIL_USE_TLS=True
+ACCOUNT_ACTIVATION_DAYS=1
+DEFAULT_FROM_EMAIL='mishymmoringa@gmail.com'
+EMAIL_HOST_PASSWORD='@Mypassword'
+EMAIL_HOST_USER='mishymmoringa@gmail.com'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
 EMAIL_USE_TLS=True
+
 
 # #cloudinary configs
 # cloud_name=config('cloud_name')
 # api_key=config('api_key')
 # api_secret=config('api_secret')
+
+cloudinary.config(
+    cloud_name='mishmish',
+    api_key='739237125342173',
+    api_secret='0m3FpTW7VNcn3l6_bcja3ztpscw',
+)
+
+SECRET_KEY='django-insecure-z-6qr#twb()*gv$tqewsfm2(841-ixss+wwnwq+^i=kj3c#j*t'

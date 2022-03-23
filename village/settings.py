eb2f435fc664d6114688ed154238ab8c2f0f13a7
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-03-23 08:23:53 +0300

    a few updates

--- village/settings.py
+++ village/settings.py
@@ -145,20 +145,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL= 'profile'
 
 # Get .env variables
-# SECRET_KEY =os.environ.get('SECRET_KEY')
-# ACCOUNT_ACTIVATION_DAYS= os.environ.get('ACCOUNT_ACTIVATION_DAYS')
-# DEFAULT_FROM_EMAIL=os.environ.get('DEFAULT_FROM_EMAIL')
-# EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_HOST_PASSWORD')
-# EMAIL_HOST_USER=os.environ.get('EMAIL_HOST_USER')
-# EMAIL_HOST=os.environ.get('EMAIL_HOST')
-# EMAIL_PORT= os.environ.get('EMAIL_PORT')
-# EMAIL_USE_TLS=True
-ACCOUNT_ACTIVATION_DAYS=1
-DEFAULT_FROM_EMAIL='mishymmoringa@gmail.com'
-EMAIL_HOST_PASSWORD='@Mypassword'
-EMAIL_HOST_USER='mishymmoringa@gmail.com'
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_PORT=587
+SECRET_KEY =os.environ.get('SECRET_KEY')
+ACCOUNT_ACTIVATION_DAYS= os.environ.get('ACCOUNT_ACTIVATION_DAYS')
+DEFAULT_FROM_EMAIL=os.environ.get('DEFAULT_FROM_EMAIL')
+EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER=os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST=os.environ.get('EMAIL_HOST')
+EMAIL_PORT= os.environ.get('EMAIL_PORT')
 EMAIL_USE_TLS=True
 
 

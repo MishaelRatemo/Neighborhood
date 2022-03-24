aeee1f33233e39e4462b4c6a81a12d053a60abe0
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-03-24 14:31:45 +0300

    db  updates

--- village/settings.py
+++ village/settings.py
@@ -120,7 +120,7 @@ WSGI_APPLICATION = 'village.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'NAME': 'neighborhood',
         'USER': 'moringa',
